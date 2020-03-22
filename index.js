cd1de8282ee00eb7a67fa348b4e78437def77f47
Chris Sanchez <chrisvsanchez@gmail.com>
2020-03-22 18:52:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function increasedRankBy(n){
   const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
   
   for (let i = 0; i < upRank.length; i++){
-    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML))+n);
   }
 }
