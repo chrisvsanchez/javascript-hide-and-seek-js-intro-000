17032bba53a384343e9571c6cc05f3e69dc90277
Chris Sanchez <chrisvsanchez@gmail.com>
2020-03-22 19:10:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function deepestChild(){
 }
 
 function increaseRankBy(n){
-  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
   
   for (let i = 0; i < upRank.length; i++){
-    upRank[i].innerHTML = parseInt(upRank[i].innerHTML) + n;
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
   }
   
 }
